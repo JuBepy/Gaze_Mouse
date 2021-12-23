be62ccf4cc81ebce34d6a6979327fbd252c0b20a
CyrilleFinet3 <79786800+CyrilleFinet3@users.noreply.github.com>
2021-12-23 11:44:43 +0100

    Update __main__.py

--- merge/__main__.py
+++ merge/__main__.py
@@ -9,7 +9,10 @@ from queue import Queue
 
 ### Libraries for the mouse and speech recognition
 import mouse
-import speech_recognition as sr
+import sounddevice as sd
+import vosk
+import argparse
+import json
 
 
 if getattr(sys, "frozen", False) and hasattr(sys, "_MEIPASS"):
@@ -23,34 +26,136 @@ from .texture import PITextureController
 from .ui import HostViewController
 from .window import Window
 
-def audio_recognition (buffer_mouse_click, buffer_mouse_option):
-    
-    command = ''
-    
-    while True:
+
+
+def audio_recognition (buffer_mouse_click, buffer_mouse_option, args, q_vosk, model, dump_fn):
         
-        r = sr.Recognizer()
-        mic = sr.Microphone()
-        sr.Microphone.list_microphone_names()
+   
+    
+    with sd.RawInputStream(samplerate=args.samplerate, blocksize = 8000, device=args.device, dtype='int16',
+                        channels=1, callback=callback):
+      
+
+        rec = vosk.KaldiRecognizer(model, args.samplerate)
+        while True:
+            data = q_vosk.get()
+            if rec.AcceptWaveform(data):
+                
+                dictResult = json.loads(rec.Result())
+                 
+                if "sélection" in dictResult.get("text"):
+                      print("[LEFT CLICK]")
+                      mouse.click("left")
+                    # buffer_mouse_click.put("sélection")
+                
+                if "option" in dictResult.get("text"):        
+                    print("[RIGHT CLICK]")
+                    mouse.click("right")
+                    # buffer_mouse_option.put("option")
+                    
+                if "montée" in dictResult.get("text"):
+                    print("[UP]")
+                    mouse.wheel(1)
+                    
+                if "descente" in dictResult.get("text"):
+                    print("[DOWN]")
+                    mouse.wheel(-1)
+                    
+            else:
+                pass
+               # print(rec.PartialResult())
+            if dump_fn is not None:
+                dump_fn.write(data)
+   
+
+if __name__ == "__main__":
+    
+    
+    q_vosk = Queue()
+
+    def int_or_str(text):
+        """Helper function for argument parsing."""
+        try:
+            return int(text)
+        except ValueError:
+            return text
+    
+    def callback(indata, frames, time, status):
+        """This is called (from a separate thread) for each audio block."""
+        if status:
+            print(status, file=sys.stderr)
+        q_vosk.put(bytes(indata))
+    
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument(
+        '-l', '--list-devices', action='store_true',
+        help='show list of audio devices and exit')
+    args, remaining = parser.parse_known_args()
+    if args.list_devices:
+        print(sd.query_devices())
+        parser.exit(0)
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        parents=[parser])
+    parser.add_argument(
+        '-f', '--filename', type=str, metavar='FILENAME',
+        help='audio file to store recording to')
+    parser.add_argument(
+        '-m', '--model', type=str, metavar='MODEL_PATH',
+        help='Path to the model')
+    parser.add_argument(
+        '-d', '--device', type=int_or_str,
+        help='input device (numeric ID or substring)')
+    parser.add_argument(
+        '-r', '--samplerate', type=int, help='sampling rate')
+    args = parser.parse_args(remaining)
+    
+    
+    
+    if args.model is None:
+        args.model = "model"
+    if not os.path.exists(args.model):
+        print ("Please download a model for your language from https://alphacephei.com/vosk/models")
+        print ("and unpack as 'model' in the current folder.")
+        parser.exit(0)
+    if args.samplerate is None:
+        device_info = sd.query_devices(args.device, 'input')
+        # soundfile expects an int, sounddevice provides a float:
+        args.samplerate = int(device_info['default_samplerate'])
+    
+    model = vosk.Model(args.model)
+    print('MODELLLLLLLLLLLLLLLLL :'+ str(model))
+    if args.filename:
+        dump_fn = open(args.filename, "wb")
+    else:
+        dump_fn = None
+    
+    def click_function (buffer_mouse_click, buffer_mouse_option):
         
-        with mic as source:
-            audio = r.listen(source)
-    
-        command = str(r.recognize_google(audio))
-        #print(command)
+        if buffer_mouse_click.empty() == False:
+            buffer_mouse_click.get()
+            print("[LEFT CLICK]")
+            mouse.click("left")
         
-        if command == 'select':
-            buffer_mouse_click.put("1")
-            
-        if command == 'option':
-            buffer_mouse_option.put("1")
+        if buffer_mouse_option.empty() == False:
+            buffer_mouse_option.get()
+            print("[RIGHT CLICK]")
+            mouse.click("right")
+        
     
-
-
-
-
-def main(buffer_mouse_click, buffer_mouse_option):
+     
+    ### Threading ###
     
+    buffer_mouse_click = Queue() # buffer click gauche souris
+    buffer_mouse_option = Queue() # buffer click droit souris
+    
+    thr = threading.Thread(target = audio_recognition, args = (buffer_mouse_click, buffer_mouse_option,args, q_vosk, model, dump_fn,) )
+    thr.start()
+    
+    
+    # main(buffer_mouse_click, buffer_mouse_option)
+        
     log_path = Path.home() / "pi_monitor_settings" / "pi_monitor.log"
     log_path.parent.mkdir(exist_ok=True)
     handlers = [
@@ -77,19 +182,21 @@ def main(buffer_mouse_click, buffer_mouse_option):
 
         gaze_overlay = GazeOverlay()
         
+        
         # Ajouts Cyrille
         # coordinates = np.array (gaze_overlay._recent_gaze())
         # mouse.move(coordinates[0], coordinates[1], absolute=True, duration=0)
         
-        if buffer_mouse_click.empty() == False:
-            buffer_mouse_click.get()
-            print("[LEFT CLICK]")
-            mouse.click("left")
+        # if buffer_mouse_click.empty() == False:
+        #     buffer_mouse_click.get()
+        #     print("[LEFT CLICK]")
+        #     mouse.click("left")
         
-        if buffer_mouse_option.empty() == False:
-            buffer_mouse_option.get()
-            print("[RIGHT CLICK]")
-            mouse.click("right")
+        # if buffer_mouse_option.empty() == False:
+        #     print("NOICE")
+        #     buffer_mouse_option.get()
+        #     print("[RIGHT CLICK]")
+        #     mouse.click("right")
             
         host_controller.add_observer("on_recent_gaze", gaze_overlay.update)
         
@@ -100,8 +207,8 @@ def main(buffer_mouse_click, buffer_mouse_option):
             callables=[
                 host_controller.poll_events,
                 host_controller.fetch_recent_data,
-                gaze_overlay.draw,
-                gaze_overlay.mouse_control,
+                gaze_overlay.draw
+             
             ],
         )
         win.open()
@@ -124,17 +231,3 @@ def main(buffer_mouse_click, buffer_mouse_option):
         host_view_controller.cleanup()
         logging.shutdown()
 
-
-if __name__ == "__main__":
-    
-     
-    ### Threading ###
-    
-    buffer_mouse_click = Queue() # buffer click gauche souris
-    buffer_mouse_option = Queue() # buffer click droit souris
-    
-    thr = threading.Thread(target = audio_recognition, args = (buffer_mouse_click, buffer_mouse_option,) )
-    thr.start()
-    
-    
-    main(buffer_mouse_click, buffer_mouse_option)
