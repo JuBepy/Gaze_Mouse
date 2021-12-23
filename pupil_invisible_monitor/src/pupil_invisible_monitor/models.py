9b111aedacd32eb7ae0ac526c362cff81468734a
benoit1906 <81262129+benoit1906@users.noreply.github.com>
2021-12-23 12:01:38 +0100

    Update models.py

--- pupil_invisible_monitor/src/pupil_invisible_monitor/models.py
+++ pupil_invisible_monitor/src/pupil_invisible_monitor/models.py
@@ -39,7 +39,7 @@ ap = argparse.ArgumentParser()
 # ap.add_argument("-i", "--image", required=True,
 #     help="path to input image containing ArUCo tag")
 ap.add_argument("-t", "--type", type=str,
-    default="DICT_ARUCO_ORIGINAL",
+    default="DICT_5X5_100",
     help="type of ArUCo tag to detect")
 args = vars(ap.parse_args())
 
